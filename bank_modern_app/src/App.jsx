bed129958bc452807ff5f5d36d74aa43f3195d41
Travis Ho <114096758+TravisHo9@users.noreply.github.com>
2023-11-30 13:56:18 +0000

    Fixed assets folder and created NavBar

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,6 +1,6 @@
 import styles from './style';
 
-import { NavBar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer} from './components';
+import {NavBar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer} from './components';
 
 const App = () => (
     <div className= "bg-primary w-full overflow-hidden">
